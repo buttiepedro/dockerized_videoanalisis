312549e49c31fd3ad02ecb5a6bc35d8a29265c96
buttiepedro <buttiepedro.bit@gmail.com>
2025-04-18 19:49:16 -0300

    Implementacion de todos los cortes falta configurar el reproductor de cortes

--- web/bitr/cortes.py
+++ web/bitr/cortes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, jsonify
 from bitr.auth import login_required
 
 from bitr.videoanalisis_funciones import get_video_link,extraer_video_id, obtener_segmentos
@@ -13,14 +13,32 @@ def cortes(id):
 
     return render_template('bit_videoanalisis/cortes/elegir_cortes.html', id=id)
 
-@bp.route('/lines/<int:id>', methods = ["GET"])
+@bp.route('lines/<int:id>', methods = ["GET"])
 @login_required
 def lines(id):
 
     return render_template('bit_videoanalisis/cortes/lines.html', id=id)
 
-@bp.route('/scrums/<int:id>', methods = ["GET"])
+@bp.route('scrums/<int:id>', methods = ["GET"])
 @login_required
 def scrums(id):
 
     return render_template('bit_videoanalisis/cortes/scrums.html', id=id)
+
+@bp.route('/line_cortes/<int:id>', methods = ["POST"])
+@login_required
+def line_cortes(id):
+
+    data = request.get_json()
+    tipo = data.get('tipo')
+
+    return render_template('bit_videoanalisis/cortes/cortes.html', id=id)
+
+@bp.route('/scrum_cortes/<int:id>', methods = ["POST"])
+@login_required
+def scrum_cortes(id):
+
+    data = request.get_json()
+    tipo = data.get('tipo')
+
+    return render_template('bit_videoanalisis/cortes/cortes.html', id=id)
